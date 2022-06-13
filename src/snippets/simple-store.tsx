5171a9417fb08a76d984d6b2575f093d2d7a1b0b
Adrian Połubiński <polubis@wp.pl>
2022-06-13 10:04:29 +0200

    Fix typos

--- src/snippets/simple-store.tsx
+++ src/snippets/simple-store.tsx
@@ -10,11 +10,11 @@ export const ChatStore = (initMessages: ChatMessage[] = []) => {
   const messagesSubject = new BehaviorSubject(initMessages);
 
   return {
-    // Hiding  implementation details
+    // Hiding implementation details
     // For consumers, we would like to expose only API for subscription
     messages$: messagesSubject.asObservable(),
 
-    // The function reading to read data without subscription
+    // The function used to read data without subscription
     // Helpful in comparison logic, ...etc
     get messages() {
       return messagesSubject.getValue();
